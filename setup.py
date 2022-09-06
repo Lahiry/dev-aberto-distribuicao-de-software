eecdad4b7f0a5d3fe890b0f43c325d4e921dde8d
Lahiry <raphaellcmb@al.insper.edu.br>
2022-09-06 11:47:07 -0300

    adding package setup file

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='Lahiry',
+      version='0.1',
+      packages=['dev_aberto'],
+      install_requires=['requests'],
+      scripts=['scripts/hello.py']
+ )
