0f786d45e9bda109c2eef1d12c810947ad86a80d
Lahiry <raphaellcmb@al.insper.edu.br>
2022-09-22 10:52:15 -0300

    internacionaliazcao

--- scripts/hello.py
+++ scripts/hello.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
 import dev_aberto
 
+from dateutil import parser
+from babel.dates import format_date
+
+import gettext
+gettext.install('hello', localedir='locale') 
+# cli é o nome do arquivo em que guardamos nossas traduções
+# localedir é o caminho onde estão armazenadas as traduções. Pode ser um caminho relativo. 
+
 if __name__ == '__main__':
     date, name = dev_aberto.hello()
-    print('Último commit feito em:', date, ' por', name)
+    last_commit = _('Último commit feito em: ')
+    by = _('por ')
+    print(last_commit, format_date(parser.parse(date), 'long'), by, name)
